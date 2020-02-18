388affcbd91accb8b3b4cc2f1c7c634bd4f1f7f9
Alexander Bedulin <alexander.bedulin@gmail.com>
2020-02-18 08:44:35 +0300

    init

---
+++ app/src/main/java/com/example/module/Module.kt
@@ -0,0 +1,13 @@
+package com.example.module
+
+
+/**
+ * Created by Alexander Bedulin on 18-Feb-20.
+ * Ziggeo, Inc.
+ * alexb@ziggeo.com
+ */
+class Module {
+    override fun toString(): String {
+        return "Module"
+    }
+}
